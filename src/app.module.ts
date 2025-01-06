e17d77c4c0b21db1a5cd56672d003d0722958f2f
Evandro Rodrigues Coelho <evandrorodcoelho@gmail.com>
2025-01-06 10:55:54 -0300

    Base project

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,15 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
-import { AppService } from './app.service';
+import { MailService } from './mail/mail.service';
+import { SMTPMailService } from './mail/smtp-mail.service';
+import { HttpModule } from './http.module';
 
 @Module({
-  imports: [],
+  imports: [HttpModule],
   controllers: [AppController],
-  providers: [AppService],
+  providers: [{
+    provide: MailService,
+    useClass: SMTPMailService
+  }],
 })
-export class AppModule {}
+export class AppModule { }
